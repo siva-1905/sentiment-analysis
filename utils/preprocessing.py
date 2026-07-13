65b6e62d26a00d13168a16f804af4afa0dba6cbd
siva-1905 <sivapriyaanandhakumar@gmail.com>
2026-07-13 16:13:11 +0530

    Fix NLTK resources for Render

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,7 +1,22 @@
 import re
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+import nltk
 
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
 stop_words = set(stopwords.words("english"))
 lemmatizer = WordNetLemmatizer()
 
